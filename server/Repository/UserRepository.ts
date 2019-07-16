c508051441fa828a46cc360a3c59f223218d5711
Chris Jantzen <jantzeca@mail.uc.edu>
2019-07-15 21:41:49 -0400

    createUser implemented

--- server/Repository/UserRepository.ts
+++ server/Repository/UserRepository.ts
@@ -1,11 +1,12 @@
-import { model } from 'mongoose';
+import { model, Model, Document } from 'mongoose';
 import { userSchema } from '../Models/User';
 
-const User = model('User', userSchema);
+const User = model('User', userSchema, 'user');
 
 export class UserRepository {
   public createUser = (userInfo: any, response: Function) => {
     const newUser = new User(userInfo);
+    console.log(newUser);
     newUser.save((err: any, user: any) => {
       return response(err, {
         fname: user.fname,
